376325e9a22b4a37bb43be8b1f616594024cf163
VuNT <nguyenthachvu.vn@gmail.com>
2019-04-30 13:40:58 +0700

    add quy trinh don vi services

--- src/main/java/com/manager/donviphathanh/client/QuyTrinhDonViServiceClient.java
+++ src/main/java/com/manager/donviphathanh/client/QuyTrinhDonViServiceClient.java
@@ -2,7 +2,7 @@ package com.manager.donviphathanh.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "common", qualifier = "common", url = "${application.gateway.quytrinhdonvi}", path = "api", fallback = QuyTrinhDonViServiceClientFallback.class)
+@FeignClient(name = "quytrinhdonvi", qualifier = "quytrinhdonvi", url = "${application.gateway.quytrinhdonvi}", path = "api", fallback = QuyTrinhDonViServiceClientFallback.class)
 public interface QuyTrinhDonViServiceClient {
 
 }
